ec148d4a8db83621d18a83d50de9468a3a2025ff
Lupe13 <lupe.flores.r@gmail.com>
2022-08-03 11:17:50 +0200

    UserDetails test

---
+++ src/test/java/com/recruit/recruitment/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.recruit.recruitment.service;
+
+import com.recruit.recruitment.models.AppUser;
+import com.recruit.recruitment.repository.AppUserRepo;
+import com.recruit.recruitment.repository.RoleRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserDetailsServiceImplTest {
+
+    @Mock
+    private AppUserRepo appUserRepo;
+
+    @Mock
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private RoleRepo roleRepo;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    private AppUserServiceImpl underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new AppUserServiceImpl(appUserRepo, roleRepo);
+    }
+
+    @Test
+    void loadUserByUsername() {
+        AppUser appUser = new AppUser(
+
+                1L,
+                "user",
+                "user",
+                "user@mail.com",
+                "password",
+                true
+        );
+        given(appUserRepo.findAppUserByUsername("user")).willReturn(Optional.of(appUser));
+
+        underTest.findByUsername("user");
+        verify(appUserRepo).findAppUserByUsername("user");
+/*
+
+        given(userDetailsService.loadUserByUsername("user")).willReturn()
+        userDetailsService.loadUserByUsername("user");
+*/
+
+
+    }
+}
