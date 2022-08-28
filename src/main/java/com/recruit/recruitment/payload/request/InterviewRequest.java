20e3a4f5a541a6f08fef374f6d1c2af6adf293e9
checkmyprojects <notnow.plz+github@gmail.com>
2022-08-28 21:56:13 +0200

    interview now has a state

---
+++ src/main/java/com/recruit/recruitment/payload/request/InterviewRequest.java
@@ -0,0 +1,59 @@
+package com.recruit.recruitment.payload.request;
+
+public class InterviewRequest {
+
+    private Long candidateId;
+    private Long interviewerId;
+    private Long selectionId;
+    private String date;
+    private String status;
+    private String feedback;
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public Long getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(Long candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public Long getInterviewerId() {
+        return interviewerId;
+    }
+
+    public void setInterviewerId(Long interviewerId) {
+        this.interviewerId = interviewerId;
+    }
+
+    public Long getSelectionId() {
+        return selectionId;
+    }
+
+    public void setSelectionId(Long selectionId) {
+        this.selectionId = selectionId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
