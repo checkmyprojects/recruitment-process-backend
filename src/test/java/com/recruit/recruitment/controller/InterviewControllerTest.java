b5882dc7f4fbfb6ee4a36a5224468896b9ef0a58
checkmyprojects <notnow.plz+github@gmail.com>
2022-08-29 15:33:41 +0200

    interview controller half tested

--- src/test/java/com/recruit/recruitment/controller/InterviewControllerTest.java
+++ src/test/java/com/recruit/recruitment/controller/InterviewControllerTest.java
@@ -1,16 +1,29 @@
 package com.recruit.recruitment.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.recruit.recruitment.models.Interview;
+import com.recruit.recruitment.models.Selection;
 import com.recruit.recruitment.service.AppUserServiceImpl;
 import com.recruit.recruitment.service.CandidateServiceImpl;
 import com.recruit.recruitment.service.InterviewServiceImpl;
 import com.recruit.recruitment.service.SelectionServiceImpl;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,54 +37,56 @@ import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class InterviewControllerTest {
 
-    private final InterviewServiceImpl interviewService = Mockito.mock(InterviewServiceImpl.class);
-    private final AppUserServiceImpl appUserService = Mockito.mock(AppUserServiceImpl.class);
-    private final SelectionServiceImpl selectionService = Mockito.mock(SelectionServiceImpl.class);
-    private final CandidateServiceImpl candidateService = Mockito.mock(CandidateServiceImpl.class);
+    @MockBean
+    private InterviewServiceImpl interviewService;
+    @MockBean
+    private AppUserServiceImpl appUserService;
+    @MockBean
+    private SelectionServiceImpl selectionService;
+    @MockBean
+    private CandidateServiceImpl candidateService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     private InterviewController interviewController = new InterviewController(interviewService, appUserService, selectionService, candidateService);
 
     @Test
-    void allList() {
+    void allList() throws Exception {
         List<Interview> interviewList= new ArrayList<>();
-        InterviewController interviewController = new InterviewController(interviewService, appUserService, selectionService, candidateService);
-        given(interviewService
-                .listAll())
-                .willReturn(interviewList);
-        ResponseEntity<List<Interview>> response = interviewController
-                .allList();
+        Interview interview= new Interview(1L, null, null, null, "Good Good", null, null);
+        interviewList.add(interview);
 
-        then(interviewService)
-                .should()
-                .listAll();
+        when(interviewService.listAll()).thenReturn(interviewList);
+        mockMvc.perform((MockMvcRequestBuilders.get("/api/interview/list")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(interviewList))))
+                .andDo(MockMvcResultHandlers.print());
+        verify(interviewService).listAll();
+        verify(interviewService, times(1)).listAll();
 
-        assertThat(response.getStatusCode())
-                .isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    void findById() {
-        Interview interview= new Interview(1L, null, null, null, "Good Good", LocalDateTime.now(), LocalDateTime.now());
-        InterviewController interviewController = new InterviewController(interviewService, appUserService, selectionService, candidateService);
-        given(interviewService
-                .findById(interview.getId()))
-                .willReturn(interview);
+    void findById() throws Exception {
+        Interview interview= new Interview(1L, null, null, null, "Good Good", null, null);
 
-        ResponseEntity<Interview> response = interviewController
-                .findById(interview.getId());
-
-        then(interviewService)
-                .should()
-                .findById(interview.getId());
-
-        assertThat(response.getStatusCode())
-                .isEqualTo(HttpStatus.OK);
-        //JSONAssert.assertEquals(interview);
-
-        assertThat(response.getBody()).hasFieldOrProperty("feedback");
+        when(interviewService.findById(any())).thenReturn(interview);
+        mockMvc.perform((MockMvcRequestBuilders.get("/api/interview/id/"+interview.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(interview))))
+                .andDo(MockMvcResultHandlers.print());
+        verify(interviewService).findById(interview.getId());
+        verify(interviewService, times(1)).findById(interview.getId());
     }
 
     @Test
@@ -83,14 +98,51 @@ class InterviewControllerTest {
     }
 
     @Test
-    void leaveFeedback() {
+    void leaveFeedback() throws Exception {
+        Interview interview= new Interview(1L, null, null, null, "Good Good", null, null);
+        when(interviewService.findById(any())).thenReturn(interview);
+        mockMvc.perform((MockMvcRequestBuilders.put("/api/interview/feedback/"+interview.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString("Feedback"))))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated());
+        verify(interviewService).findById(1L);
+        verify(interviewService, times(1)).findById(1L);
+        verify(interviewService).save(interview);
+        verify(interviewService, times(1)).save(interview);
     }
 
     @Test
-    void changeDate() {
+    void changeDate() throws Exception {
+        Interview interview= new Interview(1L, null, null, null, "Good Good", null, null);
+        when(interviewService.findById(any())).thenReturn(interview);
+        mockMvc.perform((MockMvcRequestBuilders.put("/api/interview/changedate/")
+                        .param("interviewid", "1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString("2022-12-12T05:05:05"))))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());;
+        verify(interviewService).findById(1L);
+        verify(interviewService, times(1)).findById(1L);
     }
 
     @Test
-    void deleteInterviewById() {
+    void deleteInterviewById() throws Exception {
+        Interview interview= new Interview(1L, null, null, null, "Good Good", null, null);
+        mockMvc.perform((MockMvcRequestBuilders.delete("/api/interview/delete/"+interview.getId())
+                        .contentType(MediaType.APPLICATION_JSON)))
+                .andDo(MockMvcResultHandlers.print());
+        verify(interviewService).deleteInterviewById(interview.getId());
+        verify(interviewService, times(1)).deleteInterviewById(interview.getId());
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
